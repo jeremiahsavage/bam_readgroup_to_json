d7c0f25fc918b15b63c0700bbeea43ab94f60d21
Jeremiah H. Savage <jeremiahsavage@gmail.com>
2016-10-05 12:50:52 -0500

    allow PU to be missing form legacy data

--- bam_readgroup_to_json/main.py
+++ bam_readgroup_to_json/main.py
@@ -30,6 +30,25 @@ def check_readgroup(readgroup_dict, logger):
     # if not 'DT' in readgroup_dict
     return
 
+def legacy_check_readgroup(readgroup_dict, logger):
+    if not 'CN' in readgroup_dict:
+        logger.info('"CN" is missing from readgroup: %s' % readgroup_dict)
+        sys.exit(1)
+    if not 'ID' in readgroup_dict:
+        logger.info('"ID" is missing from readgroup: %s' % readgroup_dict)
+        sys.exit(1)
+    if not 'LB' in readgroup_dict:
+        logger.info('"LB" is missing from readgroup: %s' % readgroup_dict)
+        sys.exit(1)
+    if not 'PL' in readgroup_dict:
+        logger.info('"PL" is missing from readgroup: %s' % readgroup_dict)
+        sys.exit(1)
+    if not 'SM' in readgroup_dict:
+        logger.info('"SM" is missing from readgroup: %s' % readgroup_dict)
+        sys.exit(1)
+    # if not 'DT' in readgroup_dict
+    return
+
 def extract_readgroup_json(bam_path, logger):
     step_dir = os.getcwd()
     bam_file = os.path.basename(bam_path)
@@ -84,7 +103,7 @@ def legacy_extract_readgroup_json(bam_path, logger):
     else:
         for readgroup_dict in readgroup_dict_list:
             logger.info('readgroup_dict=%s' % readgroup_dict)
-            check_readgroup(readgroup_dict, logger)
+            legacy_check_readgroup(readgroup_dict, logger)
             readgroup_json_file = readgroup_dict['ID'] + '.json'
             logger.info('readgroup_json_file=%s\n' % readgroup_json_file)
             with open(readgroup_json_file, 'w') as f:
